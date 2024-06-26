a5eff91edd7c4206444ac1338eff072bc4816131
“Nanera <dalsukh.n@optimumfintech.co.in>
2024-06-26 19:04:05 +0530

    26-06-2024 Last

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native'
 
@@ -17,20 +17,24 @@ import Home from './src/screen/Home.js/Home';
 import Mpin from './src/screen/Mpin.js/Mpin';
 import Webview from './src/screen/WebView/Webview';
 import SetPin from './src/screen/SetPin/SetPin';
+import Routes from './src/Navigation/Routes';
 
-const stack = createStackNavigator();
-
-
-const  App = ()=> {
+const Stack = createStackNavigator();
 
 
+const  App = (props)=> {
+
+
+  const [mpinCheck, setmpinCheck] = useState(false)
   useEffect(() => {
 
     (async () => {
       try {
         const value = await AsyncStorage.getItem('@Data');
         console.log('i am printing the values: ', value);
+        setmpinCheck(value ? true: false)
         if (value !== null) {
+          
           console.log('i am redirecting');
           let data = await JSON.parse(value);
           console.log('this is last ', data);
@@ -46,16 +50,15 @@ const  App = ()=> {
   }, []);
 
   return (
-  <NavigationContainer>
-    <stack.Navigator>
-    <stack.Screen name="Home" component={Home}   options={{headerShown: false}}/>
-    <stack.Screen name="Mpin" component={Mpin} />
-    <stack.Screen name="Webview" component={Webview} />
-    <stack.Screen name="SetPin" component={SetPin} />
-    </stack.Navigator>
-  </NavigationContainer>
-
-   
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={{ animation: 'none' }}>
+            <>
+            {
+                    (Routes(Stack,props.Flag))
+            }
+            </>
+          </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 /**
  * @format
  */
-
+import React from 'react'
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-AppRegistry.registerComponent(appName, () => App);
+
+const index = () =>{
+
+}
+    AppRegistry.registerRunnable(appName, async initialProps => {
+        try {  
+            const value = await AsyncStorage.getItem('@Data');
+            const props = {Flag:value?true:false };
+            const MyComponent = () => <App {...props} />;
+              AppRegistry.registerComponent(appName, () => MyComponent);
+             AppRegistry.runApplication(appName, initialProps);
+        } catch (err) {
+          console.log(err);
+        }
+      });
+
+
+export default index;
+
+// AppRegistry.registerComponent(appName, () => App);
